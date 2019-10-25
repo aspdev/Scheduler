9813347e93da8f6d784a51e997ffa1af939e910f
Lukasz Dobrzynski <lukasz.dobrzynski@wp.pl>
2019-10-26 00:43:52 +0200

    [Scheduler] Add OverwriteSchedule

--- Scheduler/Client.Torun.RavenDataService/Controllers/DutyController.cs
+++ Scheduler/Client.Torun.RavenDataService/Controllers/DutyController.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Client.Torun.RavenDataService.DataStore;
 using Client.Torun.RavenDataService.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Math.EC.Rfc7748;
 using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Queries;
+using Raven.Client.Exceptions.Documents.Indexes;
 
 namespace Client.Torun.RavenDataService.Controllers
 {
@@ -21,6 +27,59 @@ namespace Client.Torun.RavenDataService.Controllers
         [HttpPost("duties")]
         public async Task<IActionResult> SaveDuties([FromBody] List<Duty> duties)
         {
+            if (duties == null)
+            {
+                return BadRequest();
+            }
+
+            using (var session = _store.OpenAsyncSession())
+            {
+                var date = duties.First().Date;
+                var dutyFromDatabase = await session.Query<Duty>().FirstOrDefaultAsync(d => d.Date.Year == date.Year && d.Date.Month == date.Month);
+
+                var message = "";
+
+                if (dutyFromDatabase != null)
+                {
+                    message = $"Schedule for {date:Y} already exists. Do you want to overwrite it?";
+                    
+                    return Ok(new {message});
+
+                }
+                
+                foreach(var duty in duties)
+                {
+                    await session.StoreAsync(duty);
+                }
+
+                await session.SaveChangesAsync();
+                
+
+                return Ok(new {message});
+            }
+        }
+
+        [HttpPost("duties/overwrite")]
+        public async Task<IActionResult> OverwriteSchedule([FromBody] List<Duty> duties)
+        {
+            if(duties == null)
+            {
+                return BadRequest();
+            }
+
+            var date = duties.First().Date;
+
+            var startDate = new DateTime(date.Year, date.Month, 1).ToString("yyyy-MM-ddT00:00:00.0000000");
+            var endDate = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month)).ToString("yyyy-MM-ddT00:00:00.0000000");
+
+            var operation =  await _store.Operations
+                .SendAsync(new DeleteByQueryOperation(new IndexQuery
+                {
+                    Query = $"from Duties where Date >= '{startDate}' and Date <= '{endDate}'"
+                }));
+
+            await operation.WaitForCompletionAsync(TimeSpan.FromSeconds(15));
+
             using (var session = _store.OpenAsyncSession())
             {
                 foreach (var duty in duties)
