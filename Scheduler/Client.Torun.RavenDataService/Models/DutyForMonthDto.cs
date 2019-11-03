6049b22ee17100dcfd32c66e0924fff9c3f5c499
Lukasz Dobrzynski <lukasz.dobrzynski@wp.pl>
2019-11-03 12:44:31 +0100

    [Scheduler] Add GetDuties

---
+++ Scheduler/Client.Torun.RavenDataService/Models/DutyForMonthDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Torun.RavenDataService.Models
+{
+    public class DutyForMonthDto
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string DoctorId { get; set; }
+    }
+}
