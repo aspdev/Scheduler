e6a900ed5a9efcf96bd91938903f559826316646
Lukasz Dobrzynski <lukasz.dobrzynski@wp.pl>
2020-01-01 21:39:18 +0100

    [Scheduler] Change Originator Controller

--- Scheduler/Client.Torun.RavenDataService/Controllers/OriginatorController.cs
+++ Scheduler/Client.Torun.RavenDataService/Controllers/OriginatorController.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Client.Torun.RavenDataService.Helpers;
+using Common;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Client.Torun.RavenDataService.Controllers
@@ -18,29 +20,32 @@ namespace Client.Torun.RavenDataService.Controllers
     public class OriginatorController : Controller
     {
         private readonly DataServiceConfiguration _configuration;
-        private readonly IDocumentStore _store;
+        private readonly IDocumentStore _clientStore;
+        private readonly IDocumentStore _identityStore;
         private readonly IMapper _mapper;
 
-        public OriginatorController(DataServiceConfiguration configuration, ClientDocumentStoreHolder storeHolder, IMapper mapper)
+        public OriginatorController(DataServiceConfiguration configuration, 
+            ClientDocumentStoreHolder clientStoreHolder,
+            IdentityServerDocumentStoreHolder identityServerStoreHolder,
+            IMapper mapper)
         {
             _configuration = configuration;
-            _store = storeHolder.Store;
+            _clientStore = clientStoreHolder.Store;
+            _identityStore = identityServerStoreHolder.Store;
             _mapper = mapper;
         }
 
         [HttpGet("number-of-doctors-on-duty")]
         public async Task<IActionResult> GetNumberOfDoctorsOnDuty()
         {
-            using (var session = _store.OpenAsyncSession())
+            using (var clientSession = _clientStore.OpenAsyncSession())
             {
-                AdminSettings adminSettings = await session.Query<AdminSettings>().FirstAsync();
+                AdminSettings adminSettings = await clientSession.Query<AdminSettings>().FirstAsync();
 
                 int numberOfDocs = adminSettings.NumberOfDoctorsOnDuty;
 
                 return Ok(numberOfDocs);
-                
             }
-                      
         }
 
         [HttpGet("population-size")]
@@ -54,22 +59,24 @@ namespace Client.Torun.RavenDataService.Controllers
         [HttpGet("total-number-of-doctors")]
         public async Task<IActionResult> GetTotalNumberOfDoctors()
         {
-
-            using (var session = _store.OpenAsyncSession())
+            using (var identitySession = _identityStore.OpenAsyncSession())
             {
-                int totalNumberOfDoctors = await session.Query<User>().CountAsync();
+                int totalNumberOfDoctors = await identitySession.Query<IdentityServerUser>()
+                    .Where(u => u.Clients.Contains(ConstNames.TorunClientName))
+                    .CountAsync();
 
                 return Ok(totalNumberOfDoctors);
             }
-                      
         }
 
         [HttpGet("doctor-ids")]
         public async Task<IActionResult> GetDoctorIds()
         {
-            using (var session = _store.OpenAsyncSession())
+            using (var identitySession = _identityStore.OpenAsyncSession())
             {
-                var doctorIds = await session.Query<User>().Select(u => u.Id).ToListAsync();
+                var doctorIds = await identitySession.Query<IdentityServerUser>()
+                    .Where(u => u.Clients.Contains(ConstNames.TorunClientName))
+                    .Select(u => u.Id).ToListAsync();
 
                 return Ok(doctorIds);
             }
@@ -83,25 +90,19 @@ namespace Client.Torun.RavenDataService.Controllers
                 return BadRequest();
             }
 
-            using (var session = _store.OpenAsyncSession())
+            using (var clientSession = _clientStore.OpenAsyncSession())
             {
-                if (await session.Query<DayOff>().AnyAsync())
+                if (await clientSession.Query<DayOff>().AnyAsync())
                 {
-                    List<DayOffDto> daysOff = await session.Query<DayOff>()
+                    List<DayOffDto> daysOff = await clientSession.Query<DayOff>()
                         .Where(d => d.Date.Year == dateFilter.Value.Year && d.Date.Month == dateFilter.Value.Month)
                         .Select(d => new DayOffDto { UserId = d.UserId, Date = d.Date }).ToListAsync();
 
                     return Ok(daysOff);
                 }
-                
-                //List<Tuple<string, DateTime>> daysOff = await session.Query<DayOff>()
-                //     .Where(d => d.Date.Year == dateFilter.Value.Year && d.Date.Month == dateFilter.Value.Month)
-                //     .Select(d => new Tuple<string, DateTime>(d.UserId, d.Date)).ToListAsync();
 
                 return Ok(new List<DayOffDto>());
-                
             }
-                       
         }
 
         [HttpGet("public-holidays")]
@@ -161,11 +162,11 @@ namespace Client.Torun.RavenDataService.Controllers
             int currentYear = year.Value;
             int currentMonth = month.Value;
 
-            using (var session = _store.OpenAsyncSession())
+            using (var clientSession = _clientStore.OpenAsyncSession())
             {
-                if (await session.Query<DutyRequirement>().AnyAsync())
+                if (await clientSession.Query<DutyRequirement>().AnyAsync())
                 {
-                    List<DutyRequirementForMonthDto> dutyRequirements = await session.Query<DutyRequirement>()
+                    List<DutyRequirementForMonthDto> dutyRequirements = await clientSession.Query<DutyRequirement>()
                         .Where(dr => dr.Date.Year == currentYear && dr.Date.Month == currentMonth)
                         .Select(dr => new DutyRequirementForMonthDto
                         {
@@ -177,12 +178,6 @@ namespace Client.Torun.RavenDataService.Controllers
 
                     return Ok(dutyRequirements);
                 }
-                
-                //var dutyRequirements = await session.Query<DutyRequirement>()
-                //    .Where(dr => dr.Date.Year == currentYear && dr.Date.Month == currentMonth)
-                //    .Select(dr => new KeyValuePair<string, Tuple<int, int, int>>(dr.UserId,
-                //    new Tuple<int, int, int>(dr.RequiredTotalDutiesInMonth, dr.TotalWeekdayDuties, dr.TotalHolidayDuties)))
-                //    .ToListAsync();
 
                 return Ok(new List<DutyRequirementForMonthDto>());
             }
@@ -204,11 +199,11 @@ namespace Client.Torun.RavenDataService.Controllers
 
             DateTime previousMonth = new DateTime(year.Value, month.Value, 1).AddMonths(-1);
 
-            using (var session = _store.OpenAsyncSession())
+            using (var clientSession = _clientStore.OpenAsyncSession())
             {
-                if (await session.Query<Duty>().AnyAsync())
+                if (await clientSession.Query<Duty>().AnyAsync())
                 {
-                    var doctors = await session.Query<Duty>()
+                    var doctors = await clientSession.Query<Duty>()
                         .Where(d => d.Date.Year == previousMonth.Year &&
                                     d.Date.Month == previousMonth.Month &&
                                     d.Date.Day == DateTime.DaysInMonth(previousMonth.Year, previousMonth.Month))
@@ -218,7 +213,6 @@ namespace Client.Torun.RavenDataService.Controllers
                 }
 
                 return Ok(new List<string>());
-                
             }
         }
 
@@ -239,11 +233,11 @@ namespace Client.Torun.RavenDataService.Controllers
             DateTime previousMonthDate = new DateTime(year.Value, month.Value, 1).AddMonths(-1);
             int lastDay = DateTime.DaysInMonth(previousMonthDate.Year, previousMonthDate.Month);
 
-            using (var session = _store.OpenAsyncSession())
+            using (var clientSession = _clientStore.OpenAsyncSession())
             {
-                if (await session.Query<DayOff>().AnyAsync())
+                if (await clientSession.Query<DayOff>().AnyAsync())
                 {
-                    List<DayOffOnLastDayPreviousMonthDto> daysOff = await session.Query<DayOff>()
+                    List<DayOffOnLastDayPreviousMonthDto> daysOff = await clientSession.Query<DayOff>()
                         .Where(d => d.Date.Year == previousMonthDate.Year && d.Date.Month == previousMonthDate.Month &&
                                     d.Date.Day == lastDay)
                         .Select(d => new DayOffOnLastDayPreviousMonthDto { UserId = d.UserId, Date = d.Date })
@@ -252,27 +246,18 @@ namespace Client.Torun.RavenDataService.Controllers
                     return Ok(daysOff);
                 }
 
-                
-
-                //var daysOff = await session.Query<DayOff>()
-                //    .Where(d => d.Date.Year == previousMonthDate.Year && d.Date.Month == previousMonthDate.Month && d.Date.Day == lastDay)
-                //    .Select(d => new Tuple<string, DateTime>(d.UserId, d.Date))
-                //    .ToListAsync();
-
                 return Ok(new List<DayOffOnLastDayPreviousMonthDto>());
             }
-                            
         }
 
         [HttpGet("doctordtos")]
         public async Task<IActionResult> GetDoctorDtos()
         {
-
-            using (var session = _store.OpenAsyncSession())
+            using (var identitySession = _identityStore.OpenAsyncSession())
             {
-                if (await session.Query<User>().AnyAsync())
+                if (await identitySession.Query<IdentityServerUser>().AnyAsync())
                 {
-                    var users = await session.Query<User>().ToListAsync();
+                    var users = await identitySession.Query<IdentityServerUser>().ToListAsync();
 
                     var doctorDtos = _mapper.Map<List<DoctorDto>>(users);
 
--- Scheduler/Client.Torun.RavenDataService/Mappings/MappingProfile.cs
+++ Scheduler/Client.Torun.RavenDataService/Mappings/MappingProfile.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Client.Torun.RavenDataService.Entities;
-using Client.Torun.RavenDataService.Helpers;
 using Client.Torun.RavenDataService.Models;
 using Client.Torun.Shared.DTOs;
 using Common;
@@ -18,12 +17,11 @@ namespace Client.Torun.RavenDataService.Mappings
                 });
             CreateMap<IdentityServerUser, PostCreationUserToReturnDto>();
             CreateMap<IdentityServerUser, UserToReturnDto>();
-            CreateMap<User, DoctorDto>()
+            CreateMap<IdentityServerUser, DoctorDto>()
                 .ForMember(d => d.DoctorId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(d => d.Name, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
                 .ForMember(d => d.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(d => d.Roles, opt => opt.MapFrom(src => src.Roles))
-                .ForMember(d => d.Color, opt => opt.MapFrom(src => src.Color));
+                .ForMember(d => d.Roles, opt => opt.MapFrom(src => src.Roles));
             CreateMap<RequirementToSetDto, DutyRequirement>()
                 .AfterMap((s, d) => { d.Id = string.Empty; });
             CreateMap<DutyRequirement, PostCreationRequirementToReturnDto>();
